c20d4c8cc76f70f65ff192279b5595a66740f236
GerCassa <gercassa@gmail.com>
2018-08-24 13:13:46 +0100

    passed test artist has first_name

--- art_gallery/models/artist.rb
+++ art_gallery/models/artist.rb
@@ -1,1 +1,15 @@
 require_relative('../db/sql_runner')
+
+class Artist
+
+  attr_reader :id, :first_name, :last_name, :bio, :style
+
+  def initialize(info)
+    @id = info["id"].to_i
+    @first_name = info["first_name"]
+    @last_name = info["last_name"]
+    @bio = info["bio"]
+    @style = info["style"]
+  end
+
+end
--- art_gallery/models/specs/artist_spec.rb
+++ art_gallery/models/specs/artist_spec.rb
@@ -1,11 +1,21 @@
 require("minitest/autorun")
+require("minitest/rg")
 require_relative("../artist")
 
 class TestArtist < MiniTest::Test
 
   def setup
-    info = {"id" => 1, "first_name" => "Joan", "last_name" => "Miró", "bio" => "20 April 1893 – 25 December 1983. Spanish painter, sculptor, and ceramicist born in Barcelona.", "style" => "Surrealism"}
+    info = {"id" => 1,
+       "first_name" => "Joan",
+       "last_name" => "Miró",
+       "bio" => "20 April 1893 – 25 December 1983. Spanish painter, sculptor, and ceramicist born in Barcelona.",
+       "style" => "Surrealism"}
     @artist = Artist.new(info)
+  end
+
+  def test_artist_has_first_name
+    result = @artist.first_name()
+    assert_equal("Joan", result)
   end
 
 end
