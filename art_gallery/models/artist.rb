3cb535139c313ae966a65f9ee435f21652bd4875
GerCassa <gercassa@gmail.com>
2018-08-24 15:27:14 +0100

    finished artist class CRUD  :)

--- art_gallery/models/artist.rb
+++ art_gallery/models/artist.rb
@@ -47,7 +47,14 @@ class Artist
   end
 
   def delete()
+    sql = "DELETE FROM artists WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
 
+  def self.delete_all()
+    sql = "DELETE FROM artists;"
+    SqlRunner.run(sql)
   end
 
   def self.all()
